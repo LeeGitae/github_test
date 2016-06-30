d36dc11fb631a034a60fdd2b341e8d5c8bc34f0c
LeeGitae <mm1023@naver.com>
2016-06-30 17:34:07 +0900

    깃과 메이븐과 젠킨스를 연동하는 테스트 프로젝트 시작.

---
+++ src/main/java/kr/co/dataworld/hello_maven/App.java
@@ -0,0 +1,13 @@
+package kr.co.dataworld.hello_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
